c59b95a49a3629082eae2b7223c10dbc6f38d1a6
Pete <peter.r@interstateteam.com>
2020-07-06 19:38:36 +0100

    swup nav bug

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,5 @@
 
-
-
-  
-const options = {
-  plugins: [
-    new SwupBodyClassPlugin(),
-    new SwupHeadPlugin(),
-    new SwupScrollPlugin({
-      animateScroll: false,
-      doScrollingRightAway: true,
-    }),
-  ],
-  
-};
-
-const swup = new Swup(options);
-
-function init() {
-
-
-
-
-
-
-      // Target all external links to _blank, simples.
-
-      function externalLinks() {
-        for (
-          var c = document.getElementsByTagName("a"), a = 0;
-          a < c.length;
-          a++
-        ) {
-          var b = c[a];
-          b.getAttribute("href") &&
-            b.hostname !== location.hostname &&
-            (b.target = "_blank");
-        }
-      }
-      externalLinks();
-
-      // MENU SCROLL //
+// MENU SCROLL //
 
       let scrollpos = window.scrollY;
       const header = document.querySelector("nav");
@@ -65,73 +25,98 @@ function init() {
 
 
 
+  
+const options = {
+  plugins: [
+    new SwupBodyClassPlugin(),
+    new SwupHeadPlugin(),
+    new SwupScrollPlugin({
+      animateScroll: false,
+      doScrollingRightAway: true,
+    }),
+  ],
+  
+};
 
+const swup = new Swup(options);
 
+function init() {
+        // MENU SCROLL //
 
+        let scrollpos = window.scrollY;
+        const header = document.querySelector("nav");
+        const header_height = header.offsetHeight;
 
-      if (document.querySelector("#team")) {
-        function sameHeights(selector) {
-          var selector = selector || '[data-key="sameHeights"]',
-            query = document.querySelectorAll(selector),
-            elements = query.length,
-            max = 0;
-          if (elements) {
-            while (elements--) {
-              var element = query[elements];
-              if (element.clientHeight > max) {
-                max = element.clientHeight;
-              }
-            }
-            elements = query.length;
-            while (elements--) {
-              var element = query[elements];
-              element.style.height = max + "px";
-            }
+        const add_class_on_scroll = () =>
+          header.classList.add("fade-in");
+        const remove_class_on_scroll = () =>
+          header.classList.remove("fade-in");
+
+        window.addEventListener("scroll", function () {
+          scrollpos = window.scrollY;
+
+          if (scrollpos >= 200) {
+            add_class_on_scroll();
+          } else {
+            remove_class_on_scroll();
           }
-        }
 
-        sameHeights();
-      }
-    }
+          console.log(scrollpos);
+        });
+
+
+
+                  // Target all external links to _blank, simples.
+
+                  function externalLinks() {
+                    for (
+                      var c = document.getElementsByTagName("a"), a = 0;
+                      a < c.length;
+                      a++
+                    ) {
+                      var b = c[a];
+                      b.getAttribute("href") &&
+                        b.hostname !== location.hostname &&
+                        (b.target = "_blank");
+                    }
+                  }
+                  externalLinks();
+
+                  if (document.querySelector("#team")) {
+                    function sameHeights(selector) {
+                      var selector = selector || '[data-key="sameHeights"]',
+                        query = document.querySelectorAll(selector),
+                        elements = query.length,
+                        max = 0;
+                      if (elements) {
+                        while (elements--) {
+                          var element = query[elements];
+                          if (element.clientHeight > max) {
+                            max = element.clientHeight;
+                          }
+                        }
+                        elements = query.length;
+                        while (elements--) {
+                          var element = query[elements];
+                          element.style.height = max + "px";
+                        }
+                      }
+                    }
+
+                    sameHeights();
+                  }
+                }
 
 
 swup.on("contentReplaced", init);
 
 
-function unload() {
+// function unload() {
 
-       if (document.querySelector("#team")) {
-         // equal columns
-         // lovely work from
-         // https://benhowdle.im/easy-peasy-equal-heights.html
-         // see also additonal options.
 
-         function sameHeights(selector) {
-           var selector = selector || '[data-key="sameHeights"]',
-             query = document.querySelectorAll(selector),
-             elements = query.length,
-             max = 0;
-           if (elements) {
-             while (elements--) {
-               var element = query[elements];
-               if (element.clientHeight > max) {
-                 max = element.clientHeight;
-               }
-             }
-             elements = query.length;
-             while (elements--) {
-               var element = query[elements];
-               element.style.height = max + "px";
-             }
-           }
-         }
+// }
 
-         sameHeights();
-       }
-
-}
-
-swup.on("willReplaceContent", unload);
+// swup.on("willReplaceContent", unload);
 
 
 
