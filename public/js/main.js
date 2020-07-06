c22d05197ad995f3e78009c890849f322ca87c6a
Pete <peter.r@interstateteam.com>
2020-07-06 12:35:47 +0100

    swup script fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 const options = {
   plugins: [
     new SwupBodyClassPlugin(),
@@ -18,55 +13,83 @@ const options = {
 const swup = new Swup(options);
 
 function init() {
+      // Target all external links to _blank, simples.
 
-    if (document.querySelector('#team')) {
-      // equal columns
-      // lovely work from
-      // https://benhowdle.im/easy-peasy-equal-heights.html
-      // see also additonal options.
-
-      function sameHeights(selector) {
-        var selector =
-            selector ||
-            '[data-key="sameHeights"]',
-          query = document.querySelectorAll(
-            selector
-          ),
-          elements = query.length,
-          max = 0;
-        if (elements) {
-          while (elements--) {
-            var element = query[elements];
-            if (
-              element.clientHeight > max
-            ) {
-              max = element.clientHeight;
-            }
-          }
-          elements = query.length;
-          while (elements--) {
-            var element = query[elements];
-            element.style.height =
-              max + "px";
-          }
+      function externalLinks() {
+        for (
+          var c = document.getElementsByTagName("a"), a = 0;
+          a < c.length;
+          a++
+        ) {
+          var b = c[a];
+          b.getAttribute("href") &&
+            b.hostname !== location.hostname &&
+            (b.target = "_blank");
         }
       }
+      externalLinks();
 
-      sameHeights();
 
-      
+
+      if (document.querySelector("#team")) {
+        function sameHeights(selector) {
+          var selector = selector || '[data-key="sameHeights"]',
+            query = document.querySelectorAll(selector),
+            elements = query.length,
+            max = 0;
+          if (elements) {
+            while (elements--) {
+              var element = query[elements];
+              if (element.clientHeight > max) {
+                max = element.clientHeight;
+              }
+            }
+            elements = query.length;
+            while (elements--) {
+              var element = query[elements];
+              element.style.height = max + "px";
+            }
+          }
+        }
+
+        sameHeights();
+      }
     }
 
-}
+
+swup.on("contentReplaced", init);
 
 
 function unload() {
-     
-    // if (document.querySelector("#myVideo")) {
-    //   const el = document.querySelector("video");
-    //   const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
-    //   observer.observe();
-    // }
+
+       if (document.querySelector("#team")) {
+         // equal columns
+         // lovely work from
+         // https://benhowdle.im/easy-peasy-equal-heights.html
+         // see also additonal options.
+
+         function sameHeights(selector) {
+           var selector = selector || '[data-key="sameHeights"]',
+             query = document.querySelectorAll(selector),
+             elements = query.length,
+             max = 0;
+           if (elements) {
+             while (elements--) {
+               var element = query[elements];
+               if (element.clientHeight > max) {
+                 max = element.clientHeight;
+               }
+             }
+             elements = query.length;
+             while (elements--) {
+               var element = query[elements];
+               element.style.height = max + "px";
+             }
+           }
+         }
+
+         sameHeights();
+       }
 
 }
 
@@ -77,16 +100,3 @@ swup.on("willReplaceContent", unload);
 
 
 
-
-
-
-// Target all external links to _blank, simples.
-
-function externalLinks() {
-  for (var c = document.getElementsByTagName("a"), a = 0; a < c.length; a++) {
-    var b = c[a];
-    b.getAttribute("href") && b.hostname !== location.hostname && (b.target = "_blank")
-  }
-}
-;
-externalLinks();
